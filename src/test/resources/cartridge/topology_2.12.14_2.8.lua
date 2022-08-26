b8746fbf575a6da2b5a261e1c94ca35e55d495ae
Alexey Kuzin <akudiyar@gmail.com>
2022-08-26 17:24:40 -0400

    Use broadcast IP for router nodes

--- src/test/resources/cartridge/topology_2.12.14_2.8.lua
+++ src/test/resources/cartridge/topology_2.12.14_2.8.lua
@@ -2,11 +2,11 @@ cartridge = require('cartridge')
 replicasets = {{
     alias = 'app-router',
     roles = {'vshard-router', 'app.roles.api_router'},
-    join_servers = {{uri = 'localhost:3331'}}
+    join_servers = {{uri = '0.0.0.0:3331'}}
 }, {
     alias = 'app-router-second',
     roles = {'vshard-router', 'app.roles.api_router'},
-    join_servers = {{uri = 'localhost:3341'}}
+    join_servers = {{uri = '0.0.0.0:3341'}}
 }, {
     alias = 's1-storage',
     roles = {'vshard-storage', 'app.roles.api_storage'},
